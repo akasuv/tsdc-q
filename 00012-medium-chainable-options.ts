6188ca9ccef334494a59846a2fbc986c9f755749
Wei Su <cs.akasuv@gmail.com>
2022-06-11 18:27:17 +0800

    Chainable options

---
+++ 00012-medium-chainable-options.ts
@@ -0,0 +1,46 @@
+// ============= Test Cases =============
+import type { Alike, Expect } from "./test-utils";
+
+declare const a: Chainable;
+
+const result1 = a
+  .option("foo", 123)
+  .option("bar", { value: "Hello World" })
+  .option("name", "type-challenges")
+  .get();
+
+const result2 = a
+  .option("name", "another name")
+  // @ts-expect-error
+  .option("name", "last name")
+  .get();
+
+type cases = [
+  Expect<Alike<typeof result1, Expected1>>,
+  Expect<Alike<typeof result2, Expected2>>
+];
+
+type Expected1 = {
+  foo: number;
+  bar: {
+    value: string;
+  };
+  name: string;
+};
+
+type Expected2 = {
+  name: string;
+};
+
+// ============= Your Code Here =============
+type Chainable<R = {}> = {
+  option<T extends string, V extends unknown>(
+    key: T,
+    value: V
+  ): Chainable<
+    R & {
+      [P in T]: V;
+    }
+  >;
+  get(): R;
+};
