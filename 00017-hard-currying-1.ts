776948d4eaef7a894ecdb286eefff36256ee4391
Wei Su <cs.akasuv@gmail.com>
2022-06-13 16:46:55 +0800

    Replace all

---
+++ 00017-hard-currying-1.ts
@@ -0,0 +1,18 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+const curried1 = Currying((a: string, b: number, c: boolean) => true)
+const curried2 = Currying((a: string, b: number, c: boolean, d: boolean, e: boolean, f: string, g: boolean) => true)
+
+type cases = [
+  Expect<Equal<
+    typeof curried1, (a: string) => (b: number) => (c: boolean) => true
+  >>,
+  Expect<Equal<
+    typeof curried2, (a: string) => (b: number) => (c: boolean) => (d: boolean) => (e: boolean) => (f: string) => (g: boolean) => true
+  >>,
+]
+
+
+// ============= Your Code Here =============
+declare function Currying(fn: any): any
