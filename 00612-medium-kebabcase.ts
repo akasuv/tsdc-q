50de6d0d8db569e5de3c84adeb8e053f507e6931
Wei Su <cs.akasuv@gmail.com>
2022-06-18 21:26:09 +0800

    Kebabcase

---
+++ 00612-medium-kebabcase.ts
@@ -0,0 +1,23 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<KebabCase<"FooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"fooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"foo-bar">, "foo-bar">>,
+  Expect<Equal<KebabCase<"foo_bar">, "foo_bar">>,
+  Expect<Equal<KebabCase<"Foo-Bar">, "foo--bar">>,
+  Expect<Equal<KebabCase<"ABC">, "a-b-c">>,
+  Expect<Equal<KebabCase<"-">, "-">>,
+  Expect<Equal<KebabCase<"">, "">>,
+  Expect<Equal<KebabCase<"😎">, "😎">>
+];
+
+// ============= Your Code Here =============
+type KebabCase<S, R extends string = ""> = S extends `${infer A}${infer B}`
+  ? A extends Lowercase<A>
+    ? `${KebabCase<B, `${R}${A}`>}`
+    : R extends ""
+    ? `${KebabCase<B, `${R}${Lowercase<A>}`>}`
+    : `${KebabCase<B, `${R}-${Lowercase<A>}`>}`
+  : R;
