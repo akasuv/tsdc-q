0eef7008520b125e1aff4e4e401739bfd4ff853e
Wei Su <cs.akasuv@gmail.com>
2022-07-10 20:11:26 +0800

    Drop char

---
+++ 02070-medium-drop-char.ts
@@ -0,0 +1,20 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  // @ts-expect-error
+  Expect<Equal<DropChar<"butter fly!", "">, "butterfly!">>,
+  Expect<Equal<DropChar<"butter fly!", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<"butter fly!", "!">, "butter fly">>,
+  Expect<Equal<DropChar<"    butter fly!        ", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", "b">, "  u t t e r f l y ! ">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", "t">, " b u   e r f l y ! ">>
+];
+
+// ============= Your Code Here =============
+type DropChar<S, C, R extends string = ""> = S extends `${infer A}${infer B}`
+  ? A extends C
+    ? DropChar<B, C, R>
+    : DropChar<B, C, `${R}${A}`>
+  : never;
